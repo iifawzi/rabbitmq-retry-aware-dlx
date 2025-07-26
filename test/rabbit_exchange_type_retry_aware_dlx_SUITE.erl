931bde916b8b2ac192664783da3ea57e65254e6b
Fawzi Essam <iifawzie@gmail.com>
2025-07-26 20:14:57 +0200

    Testing topologies with reject reason
    Signed-off-by: Fawzi Essam <iifawzie@gmail.com>

--- test/rabbit_exchange_type_retry_aware_dlx_SUITE.erl
+++ test/rabbit_exchange_type_retry_aware_dlx_SUITE.erl
@@ -18,7 +18,8 @@ groups() ->
     {non_parallel_tests, [], [
       direct_routing_semantics_when_messages_missing_radlx_arguments,
       direct_routing_semantics_when_messages_with_radlx_arguments_but_should_not_die_yet,
-      testing_topology_two_retries_for_one_cycle
+      testing_topology_two_retries_for_one_cycle_reject_reason,
+      testing_topology_two_retries_for_two_cycles_reject_reason
     ]}
   ].
 
@@ -78,42 +79,112 @@ direct_routing_semantics_when_messages_with_radlx_arguments_but_should_not_die_y
   ok = test_direct_routing_semantics_given_radlx(Config, single_queue_multiple_publishes()),
   passed.
 
-testing_topology_two_retries_for_one_cycle(Config) ->
-  %% Testing a topology with a Radlx exchange, a queue with a dead letter exchange,
-  %% The message should be processed by the queue two times, and then moves to the final dead letter queue.
-  {_Conn, Ch} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
+testing_topology_two_retries_for_one_cycle_reject_reason(Config) ->
+  %% Testing a topology where the message should be processed by the queue two times,
+  %% and then moves to the final dead letter queue.
+  {_, Chan} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
   DLXExchange = <<"dlx">>,
   QueueName = <<"queue">>,
   FinalDeadLetterQueue = <<"failed_queue">>,
 
-  declare_radlx_exchange(Ch, DLXExchange),
-  declare_queue_with_dlx(Ch, QueueName, DLXExchange),
-  declare_queue(Ch, FinalDeadLetterQueue),
-  bind_queue_with_routing_key(Ch, QueueName, DLXExchange, QueueName),
-  bind_queue_with_arguments(Ch, FinalDeadLetterQueue, DLXExchange, [
+  declare_exchange(Chan, DLXExchange, <<"radlx">>),
+  declare_queue_with_dlx(Chan, QueueName, DLXExchange),
+  declare_queue(Chan, FinalDeadLetterQueue),
+  bind_queue_with_routing_key(Chan, QueueName, DLXExchange, QueueName),
+  bind_queue_with_arguments(Chan, FinalDeadLetterQueue, DLXExchange, [
     {<<"x-match">>, longstr, <<"all">>},
     {<<"radlx.dead.source">>, longstr, QueueName}
   ]),
 
   Payload = <<"p">>,
-  publish_message_with_headers(Ch, QueueName, Payload, [
+  publish_message_with_headers(Chan, QueueName, Payload, [
     {<<"radlx-max-per-cycle">>, long, 2},
     {<<"radlx-track-queue">>, longstr, QueueName}
   ]),
 
-  %%  First cycle: first reject, should go to Radlx exchange, and it decided should not die yet.
+  %%  First cycle: first reject, should go to Radlx exchange, that should decide not to die yet.
   wait_for_messages(Config, [[QueueName, <<"1">>, <<"1">>, <<"0">>]]),
-  [DTag] = consume(Ch, QueueName, [Payload]),
-  amqp_channel:cast(Ch, #'basic.reject'{delivery_tag = DTag, requeue = false}),
+  [DTag] = consume(Chan, QueueName, [Payload]),
+  amqp_channel:cast(Chan, #'basic.reject'{delivery_tag = DTag, requeue = false}),
+  wait_for_messages(Config, [[FinalDeadLetterQueue, <<"0">>, <<"0">>, <<"0">>]]),
 
   %%  First cycle: second reject, should go to Radlx exchange, and given it died MaxPerCycle times,
   %%  it should be send to the final dead letter queue.
   wait_for_messages(Config, [[QueueName, <<"1">>, <<"1">>, <<"0">>]]),
-  [DTag2] = consume(Ch, QueueName, [Payload]),
-  amqp_channel:cast(Ch, #'basic.reject'{delivery_tag = DTag2, requeue = false}),
+  [DTag2] = consume(Chan, QueueName, [Payload]),
+  amqp_channel:cast(Chan, #'basic.reject'{delivery_tag = DTag2, requeue = false}),
 
   %%  Final dead letter queue should have the message now.
   wait_for_messages(Config, [[FinalDeadLetterQueue, <<"1">>, <<"1">>, <<"0">>]]),
+  %% This's important to ensure the message hasn't been routed to the original queue again
+  %% this's the only difference against normal headers exchange smeantics
+  %% where the message will be routed to all queue those have no args at all.
+  %% pattern matching in is_header_binding_match handles that, so we only do routing
+  %% to queues those have no routing key. (to avoid routing to the original queues)
+  wait_for_messages(Config, [[QueueName, <<"0">>, <<"0">>, <<"0">>]]),
+
+  cleanup_resources(Chan, [DLXExchange], [QueueName, FinalDeadLetterQueue]),
+  passed.
+
+
+testing_topology_two_retries_for_two_cycles_reject_reason(Config) ->
+  %% Testing a topology where the message should be processed by the queue two times, and rejected twice,
+  %% and then moves to the final dead letter queue that has TTL so messages will be routed back to the router exchange
+  %% which will direct it to the original queue and should be processed again for the same two times again and rejected,
+  %% before moving again to the final dead letter queue.
+  %% This test i'm validating the rem technique used to ensure same number of retries across cycles.
+  {_, Chan} = rabbit_ct_client_helpers:open_connection_and_channel(Config, 0),
+  RouterExchange = <<"router">>,
+  DLXExchange = <<"dlx">>,
+  QueueName = <<"queue">>,
+  FinalDeadLetterQueue = <<"failed_queue">>,
+
+  declare_exchange(Chan, RouterExchange, <<"direct">>),
+  declare_exchange(Chan, DLXExchange, <<"radlx">>),
+  declare_queue_with_dlx(Chan, QueueName, DLXExchange),
+  declare_queue_with_dlx_and_ttl(Chan, FinalDeadLetterQueue, RouterExchange, 5000),
+  bind_queue_with_routing_key(Chan, QueueName, RouterExchange, QueueName),
+  bind_queue_with_routing_key(Chan, QueueName, DLXExchange, QueueName),
+  bind_queue_with_arguments(Chan, FinalDeadLetterQueue, DLXExchange, [
+    {<<"x-match">>, longstr, <<"all">>},
+    {<<"radlx.dead.source">>, longstr, QueueName}
+  ]),
+
+  Payload = <<"p">>,
+  publish_message_with_headers(Chan, QueueName, Payload, [
+    {<<"radlx-max-per-cycle">>, long, 2},
+    {<<"radlx-track-queue">>, longstr, QueueName},
+    {<<"radlx-track-reason">>, longstr, <<"rejected">>}
+  ]),
+
+  %%  First cycle: first reject, should go to Radlx exchange, that should decide not to die yet.
+  wait_for_messages(Config, [[QueueName, <<"1">>, <<"1">>, <<"0">>]]),
+  [DTag] = consume(Chan, QueueName, [Payload]),
+  amqp_channel:cast(Chan, #'basic.reject'{delivery_tag = DTag, requeue = false}),
+  wait_for_messages(Config, [[FinalDeadLetterQueue, <<"0">>, <<"0">>, <<"0">>]]),
+
+  %%  First cycle: second reject, should go to Radlx exchange, and given it died MaxPerCycle times,
+  %%  it should be send to the final dead letter queue.
+  wait_for_messages(Config, [[QueueName, <<"1">>, <<"1">>, <<"0">>]]),
+  [DTag2] = consume(Chan, QueueName, [Payload]),
+  amqp_channel:cast(Chan, #'basic.reject'{delivery_tag = DTag2, requeue = false}),
+
+  %%  Final dead letter queue should have the message now, which will expire after 5 seconds
+  wait_for_messages(Config, [[FinalDeadLetterQueue, <<"1">>, <<"1">>, <<"0">>]]),
+  %% After 5 seconds, the message should be routed back to the original queue
+  wait_for_messages(Config, [[QueueName, <<"1">>, <<"1">>, <<"0">>]]),
+  %% Let's consume again in the second cycle, and reject twice to ensure it end up in the final dead letter queue again.
+  [DTag3] = consume(Chan, QueueName, [Payload]),
+  amqp_channel:cast(Chan, #'basic.reject'{delivery_tag = DTag3, requeue = false}),
+  wait_for_messages(Config, [[QueueName, <<"1">>, <<"1">>, <<"0">>]]),
+  wait_for_messages(Config, [[FinalDeadLetterQueue, <<"0">>, <<"0">>, <<"0">>]]),
+  %%  Second cycle: second reject, should go to Radlx exchange, and given it died MaxPerCycle times,
+  %%  it should be send to the final dead letter queue again.
+  [DTag4] = consume(Chan, QueueName, [Payload]),
+  amqp_channel:cast(Chan, #'basic.reject'{delivery_tag = DTag4, requeue = false}),
+  wait_for_messages(Config, [[FinalDeadLetterQueue, <<"1">>, <<"1">>, <<"0">>]]),
+
+  cleanup_resources(Chan, [DLXExchange], [QueueName, FinalDeadLetterQueue]),
   passed.
 
 
@@ -161,7 +232,7 @@ test_routing_with_headers(Config, {Queues, PublishKeys, ExpectedCount}, Headers)
   declare_queues_and_bindings(Chan, ExchangeName, Queues),
   publish_messages(Chan, ExchangeName, PublishKeys, Headers),
   verify_message_count(Chan, Queues, ExpectedCount),
-  cleanup_resources(Chan, ExchangeName, Queues),
+  cleanup_resources(Chan, [ExchangeName], Queues),
   ok.
 
 declare_exchange(Chan, ExchangeName) ->
@@ -210,52 +281,60 @@ get_queue_message_count(Chan, QueueName) ->
       exclusive = true}),
   Count.
 
-cleanup_resources(Chan, ExchangeName, Queues) ->
-  amqp_channel:call(Chan, #'exchange.delete'{exchange = ExchangeName}),
+cleanup_resources(Chan, ExchangeNames, Queues) ->
+  [amqp_channel:call(Chan, #'exchange.delete'{exchange = ExchangeName}) || ExchangeName <- ExchangeNames],
   [amqp_channel:call(Chan, #'queue.delete'{queue = Q}) || Q <- Queues],
   rabbit_ct_client_helpers:close_channel(Chan).
 
 
-declare_radlx_exchange(Ch, ExchangeName) ->
-  #'exchange.declare_ok'{} = amqp_channel:call(Ch, #'exchange.declare'{
+declare_exchange(Chan, ExchangeName, ExchangeType) ->
+  #'exchange.declare_ok'{} = amqp_channel:call(Chan, #'exchange.declare'{
     exchange = ExchangeName,
-    type = <<"radlx">>
+    type = ExchangeType
   }).
 
-declare_queue_with_dlx(Ch, QueueName, DLXExchange) ->
-  #'queue.declare_ok'{} = amqp_channel:call(Ch, #'queue.declare'{
-    queue = QueueName,
-    arguments = [{<<"x-dead-letter-exchange">>, longstr, DLXExchange}],
-    durable = true
-  }).
-
-declare_queue(Ch, QueueName) ->
-  #'queue.declare_ok'{} = amqp_channel:call(Ch, #'queue.declare'{
+declare_queue(Chan, QueueName) ->
+  #'queue.declare_ok'{} = amqp_channel:call(Chan, #'queue.declare'{
     queue = QueueName
   }).
 
-bind_queue_with_routing_key(Ch, QueueName, ExchangeName, RoutingKey) ->
-  #'queue.bind_ok'{} = amqp_channel:call(Ch, #'queue.bind'{
+declare_queue_with_dlx(Chan, QueueName, DLXExchange) ->
+  #'queue.declare_ok'{} = amqp_channel:call(Chan, #'queue.declare'{
+    queue = QueueName,
+    arguments = [{<<"x-dead-letter-exchange">>, longstr, DLXExchange}]
+  }).
+
+declare_queue_with_dlx_and_ttl(Chan, QueueName, DLXExchange, TTL) ->
+  #'queue.declare_ok'{} = amqp_channel:call(Chan, #'queue.declare'{
+    queue = QueueName,
+    arguments = [
+      {<<"x-message-ttl">>, long, TTL},
+      {<<"x-dead-letter-exchange">>, longstr, DLXExchange}
+    ]
+  }).
+
+bind_queue_with_routing_key(Chan, QueueName, ExchangeName, RoutingKey) ->
+  #'queue.bind_ok'{} = amqp_channel:call(Chan, #'queue.bind'{
     queue = QueueName,
     exchange = ExchangeName,
     routing_key = RoutingKey
   }).
 
-bind_queue_with_arguments(Ch, QueueName, ExchangeName, Arguments) ->
-  #'queue.bind_ok'{} = amqp_channel:call(Ch, #'queue.bind'{
+bind_queue_with_arguments(Chan, QueueName, ExchangeName, Arguments) ->
+  #'queue.bind_ok'{} = amqp_channel:call(Chan, #'queue.bind'{
     queue = QueueName,
     exchange = ExchangeName,
     arguments = Arguments
   }).
 
-publish_message_with_headers(Ch, RoutingKey, Payload, Headers) ->
-  amqp_channel:call(Ch, #'basic.publish'{routing_key = RoutingKey},
+publish_message_with_headers(Chan, RoutingKey, Payload, Headers) ->
+  amqp_channel:call(Chan, #'basic.publish'{routing_key = RoutingKey},
     #amqp_msg{props = #'P_basic'{headers = Headers}, payload = Payload}).
 
 
-consume(Ch, QName, Payloads) ->
+consume(Chan, QName, Payloads) ->
   [begin
      {#'basic.get_ok'{delivery_tag = DTag}, #amqp_msg{payload = Payload}} =
-       amqp_channel:call(Ch, #'basic.get'{queue = QName}),
+       amqp_channel:call(Chan, #'basic.get'{queue = QName}),
      DTag
    end || Payload <- Payloads].
